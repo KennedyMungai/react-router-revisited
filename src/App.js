3f58122faa1ef4809b313d0201d4fe2a8e90a1bb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 21:56:37 +0300

    Changed the location of the components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Products from "./components/Products";
-import Welcome from "./components/Welcome";
+import Products from "./pages/Products";
+import Welcome from "./pages/Welcome";
 
 function App()
 {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Welcome />} />
-        <Route path='/products' element={<Products />} />
-      </Routes>
-    </Router>
+    <>
+      <header>
+
+      </header>
+      <main>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Welcome />} />
+            <Route path='/products' element={<Products />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
+
   );
 }
 
