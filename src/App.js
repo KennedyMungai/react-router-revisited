3c38a4640cbab3ebff74b784bc126b2feab4f999
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-20 21:47:14 +0300

    Added a route for the Products component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App()
         <Route path='/welcome'>
           <Welcome />
         </Route>
+        <Route path='products'>
+          <Products />
+        </Route>
       </Routes>
     </Router>
   );
