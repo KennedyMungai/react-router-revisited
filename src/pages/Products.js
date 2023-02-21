78325ece7e3bd5bb09db42dfe0a36a25fb4f97e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-21 10:58:08 +0300

    Added a list of producst to the Products component

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -5,6 +5,13 @@ const Products = () =>
     return (
         <section>
             <h1>The Products Page</h1>
+            <ul>
+                <li>Product 1</li>
+                <li>Product 2</li>
+                <li>Product 3</li>
+                <li>Product 4</li>
+                <li>Product 5</li>
+            </ul>
         </section>
     )
 }
