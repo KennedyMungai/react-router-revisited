0245217c3e3b8e79d92118daca679a4f113f36b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-21 11:32:22 +0300

    Gave the products route a prop of exact

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App()
       <main>
         <Routes>
           <Route path='/' element={<Welcome />} />
-          <Route path='/products' element={<Products />} />
+          <Route path='/products' element={<Products />} exact />
           <Route path='/products/:productId' element={<ProductDetail />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
