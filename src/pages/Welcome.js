f3060bce13c78336d4eb78cba23f5545785dde50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-21 11:36:04 +0300

    Added an import for the route component

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 
 const Welcome = () =>
 {
     return (
         <section>
             <h1>Welcome To The HoneyBadger experience</h1>
+
         </section>
     )
 }
